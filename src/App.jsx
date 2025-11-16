b2a235bf28ba9715da7ee3d71c57465ea9cc87a9
Jack Johnson <jrj221@byu.edu>
2025-11-16 00:51:39 -0700

    created a 2D array to better handle indexing, which should help in evaluating game logic. converted event listeners to a transition :hover instead, which doesn't look too great yet

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import { circleFadeIn, circleFadeOut, makeMark } from "./helpers.js";
 
 function App() {
   const [mark, setMark] = useState("X");
-  const [innerGame, setInnerGame] = useState(Array(9).fill(true)); // empty inner game
+  const [outerGame, setOuterGame] = useState(Array(9).fill(Array(9).fill(true))); // 2D array of empty game boards
 
-  const handleClick = (event, index) => {
-    let element = event.currentTarget;
-    makeMark(mark, element);
-    circleFadeOut(event);
-    setMark(mark == "X" ? "O" : "X"); // swap
-    let newInnerGame = [...innerGame]; // creates a shallow copy
-    newInnerGame[index] = false;
-    setInnerGame(newInnerGame);
-  };
+  const handleClick = (event, outIndex, innerIndex) => {
+    let newOuterGame = outerGame.map((innerGame, i) => {
+        if (i === outIndex) { // is this the innerGame we were in?
+          let newInnerGame = [...innerGame]; // shallow copy of original
+          newInnerGame[innerIndex] = false;
+          let element = event.currentTarget;
+          makeMark(mark, element);
+          circleFadeOut(event);
+          setMark(mark == "X" ? "O" : "X"); // swap
+          return newInnerGame;
+        }
+        return innerGame;
+    });
+    setOuterGame(newOuterGame);
+  }
 
   const handleOver = (event) => {
     circleFadeIn(event);
@@ -30,21 +36,15 @@ function App() {
     <div className="appBody">
         <div id="board">
             <div id="outer-game">
-                <div className="inner-board">
+              {outerGame.map((innerGame, outIndex) => (
+                <div className="inner-board" key={outIndex}>
                   <div className="inner-game">
-                    {innerGame.map((square, index) => ( 
-                      <div className="select-circle" key={index} onClick={innerGame[index] ? (event) => handleClick(event, index) : undefined} onMouseOver={innerGame[index] ? handleOver : undefined} onMouseOut={innerGame[index] ? handleOut : undefined}><div className="markable_square">{index+1}</div></div>
+                    {innerGame.map((square, innerIndex) => ( 
+                      <div className="select-circle" key={innerIndex} onClick={innerGame[innerIndex] ? (event) => handleClick(event, outIndex, innerIndex) : undefined}><div className="markable_square"></div></div>
                     ))}
                   </div>
                 </div>
-                <div className="inner-board">2</div>
-                <div className="inner-board">3</div>
-                <div className="inner-board">4</div>
-                <div className="inner-board">5</div>
-                <div className="inner-board">6</div>
-                <div className="inner-board">7</div>
-                <div className="inner-board">8</div>
-                <div className="inner-board">9</div>
+              ))}
             </div>
         </div>
     </div>
